f53a5d0b18ec6d063d98932e26161900b87df2f9
Fangfei Chen <fangfei1star@gmail.com>
2019-10-20 18:34:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 def remove_element_from_end_of_array(array)
   array = [ ]
   remove_element = array.pop
-  p remove_element
+  remove_element
 end
 
 def remove_element_from_start_of_array(array)
